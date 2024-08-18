4a59bb48ba7d67d53c93bd3692360c30435b5d95
Paul Peter Callaghan <calgahan@live.co.uk>
2024-08-18 21:02:05 +0100

    chopped and changed information

--- src/pages/dis-note.jsx
+++ src/pages/dis-note.jsx
@@ -3,22 +3,15 @@ import {
   Authenticator,
   Button,
   Text,
-  TextField,
   Heading,
   Flex,
   View,
   Image,
   Grid,
-  Divider,
-  SelectField,
-  SliderField,
 } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
-import { 
-  getUrl,
-  uploadData,
-} from "aws-amplify/storage";
+import { getUrl } from "aws-amplify/storage";
 import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
 
@@ -56,31 +49,6 @@ export default function App() {
     setNotes(notes);
   }
 
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    console.log(form.get("image").name);
-
-    const { data: newNote } = await client.models.Note.create({
-      name: form.get("name"),
-      description: form.get("description"),
-      battery: form.get("battery"),
-      image: form.get("image").name,
-    });
-
-    console.log(newNote);
-    if (newNote.image)
-      if (newNote.image)
-        await uploadData({
-          path: ({ identityId }) => `media/${identityId}/${newNote.image}`,
-
-          data: form.get("image"),
-        }).result;
-
-    fetchNotes();
-    event.target.reset();
-  }
-
   async function deleteNote({ id }) {
     const toBeDeletedNote = {
       id: id,
@@ -105,51 +73,6 @@ export default function App() {
           width="100%"
           margin="0 auto"
         >
-          <Heading level={1}>Add Record</Heading>
-          <View as="form" margin="3rem 0" onSubmit={createNote}>
-            <Flex
-              direction="column"
-              justifyContent="center"
-              gap="2rem"
-              padding="2rem"
-            >
-            <SelectField label="Food" name="name"
-              descriptiveText="Select Option">
-              <option value="breakfast">Breakfast</option>
-              <option value="lunch">Lunch</option>
-              <option value="dinner">Dinner</option>
-              <option value="snack before bed">Before Bed Snack</option>
-              <option value="snack any time">Snack</option>
-            </SelectField>
-              <TextField
-                name="description"
-                placeholder="Record Note's"
-                label="Record Note's"
-                labelHidden
-                variation="quiet"
-              />
-              <SliderField
-                label="Battery Level"
-                name="battery"
-                min={0}
-                max={100}
-                step={5}
-                defaultValue={50}
-              />
-              <View
-                  name="image"
-                  as="input"
-                  type="file"
-                  alignSelf={"end"}
-                  accept="image/png, image/jpeg"
-                />
-
-              <Button type="submit" variation="primary">
-                Save Record
-              </Button>
-            </Flex>
-          </View>
-          <Divider />
           <Heading level={2}>Meal Details</Heading>
           <Grid
             margin="3rem 0"
