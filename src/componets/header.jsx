35deebcf4b82b37ea6c10816654e8c1612e1a51d
Paul Peter Callaghan <calgahan@live.co.uk>
2024-08-18 23:56:12 +0100

    aa

--- src/componets/header.jsx
+++ src/componets/header.jsx
@@ -3,11 +3,10 @@ import {
     Button,
     Text,
   } from "@aws-amplify/ui-react";
-  import Amplify from "aws-amplify";
   
-  import outputs from "@/amplify_outputs.json";
+  
 
-  Amplify.configure(outputs);
+  
 
 
 
