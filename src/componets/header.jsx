68fe7467977bbaeb58157af02b708df6297f40ff
Paul Peter Callaghan <calgahan@live.co.uk>
2024-08-18 23:47:17 +0100

    tt

--- src/componets/header.jsx
+++ src/componets/header.jsx
@@ -5,7 +5,7 @@ import {
   } from "@aws-amplify/ui-react";
   import Amplify from "aws-amplify";
   
-  import outputs from "./amplify_outputs.json";
+  import outputs from ".././amplify_outputs.json";
 
   Amplify.configure(outputs);
 
