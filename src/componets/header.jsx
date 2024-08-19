94bdc08939ae77a522a33040fb404fb54c1aea95
Paul Peter Callaghan <calgahan@live.co.uk>
2024-08-19 08:11:57 +0100

    trying to get email attributeprintUserAttributes.email);

--- src/componets/header.jsx
+++ src/componets/header.jsx
@@ -3,6 +3,17 @@ import {
     Button,
     Text,
   } from "@aws-amplify/ui-react";
+  import { fetchUserAttributes } from "@aws-amplify/auth";
+
+
+  const printUserAttributes = async () => {
+    try {
+      const userAttributes = await fetchUserAttributes();
+      console.log('Email:', userAttributes.email);
+    }
+    catch (e) { console.log(e); }
+  };
+  console.log('User attributes:', printUserAttributes.email);
 
 function Header(){
     return (
