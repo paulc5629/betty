6d9acaad72d599de9e2856834c775dc5ac688ad6
Paul Peter Callaghan <calgahan@live.co.uk>
2024-08-19 14:31:01 +0100

    string

--- src/App.jsx
+++ src/App.jsx
@@ -125,7 +125,7 @@ export default function App() {
           margin="0 auto"
         >
           <Header />
-          <Heading level={1}>Add Record {email}</Heading>
+          <Heading level={1}>Add Record {email.toString()}</Heading>
           <View as="form" margin="3rem 0" onSubmit={createNote}>
             <Flex
               direction="column"
